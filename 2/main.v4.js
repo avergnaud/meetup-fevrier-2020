857c689ebf83a5ea3e148a556008a53af26b5400
avergnaud <a.vergnaud@gmail.com>
2020-02-25 11:19:31 +0100

    Update main.v4.js
    key function

--- 2/main.v4.js
+++ 2/main.v4.js
@@ -18,6 +18,9 @@ const shuffle = () => {
     g.selectAll("text")
         //tells the browser to find the svg element and look inside it for any text. If it finds text, it returns them in a selection that is an array of elements. If it doesn’t find any, it returns an empty selection, which is what will happen in this case.
         .data(randomLetters(), d => d)
+        /*
+        A key function may be specified to control which datum is assigned to which element, replacing the default join-by-index. This key function is evaluated for each selected element, in order, being passed the current datum (d), the current index (i), and the current group (nodes), with this as the current DOM element. The key function is then also evaluated for each new datum in data, being passed the current datum (d), the current index (i), and the group’s new data, with this as the group’s parent DOM element. The datum for a given key is assigned to the element with the matching key.
+        */
         // binds data to the selection. It does this in order, so if the browser had found three text, it would link the first text to the first letter, the second to the second letter, the third one to the third letter
         // If the browser had only found two text, D3 would have put the leftover data into what’s called an enter selection
         .join(
